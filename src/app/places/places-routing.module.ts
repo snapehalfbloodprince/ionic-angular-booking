e4834ae233cdecc8e36026157c3d65430b8ee9fa
Francesco Paolo Piga <fppiga@outlook.com>
2021-01-08 15:34:37 +0100

    added tabs and routing paths

--- src/app/places/places-routing.module.ts
+++ src/app/places/places-routing.module.ts
@@ -5,17 +5,56 @@ import { PlacesPage } from './places.page';
 
 const routes: Routes = [
   {
-    path: '',
-    component: PlacesPage
+    path: 'tabs',
+    component: PlacesPage,
+    children:[
+      {
+        path: 'discover',
+        children:[
+          {path: '',
+          loadChildren: './discover/discover.module#DiscoverPageModule'
+        },
+        {
+          path: ':placeId',
+          loadChildren: './discover/place-detail/place-detail.module#PlaceDetailPageModule'
+        }
+        ]
+      },
+      {
+        path: 'offers',
+        children:[
+          {
+            path:'',
+            loadChildren: './offers/offers.module#OffersPageModule'
+          },
+          {
+            path: 'new',
+            loadChildren: './offers/new-offer/new-offer.module#NewOfferPageModule'
+          },
+          {
+            path: 'edit/:placeId',
+            loadChildren: './offers/edit-offer/edit-offer.module#EditOfferPageModule'
+          },
+          {
+            path:':placeId',
+            loadChildren: './offers/offer-bookings/offer-bookings.module#OfferBookingsPageModule'
+          },
+          {
+            path:'',
+            redirectTo: '/places/tabs/discover',
+            pathMatch:'full'
+          }
+
+        ]
+      }
+    ]
   },
   {
-    path: 'discover',
-    loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
-  },
-  {
-    path: 'offers',
-    loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule)
+    path:'',
+    redirectTo: '/places/tabs/discover',
+    pathMatch:'full'
   }
+  
 ];
 
 @NgModule({
