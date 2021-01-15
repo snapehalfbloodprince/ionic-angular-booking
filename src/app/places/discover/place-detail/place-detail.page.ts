9b386b4630491eb22a720be1f33945f5956070dc
Francesco Paolo Piga <fppiga@outlook.com>
2021-01-15 21:49:32 +0100

    added back navigation in place detail page and edit offer page

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { Place } from '../../place.model';
+import { PlacesService } from '../../places.service';
 
 @Component({
   selector: 'app-place-detail',
@@ -9,13 +11,20 @@ import { NavController } from '@ionic/angular';
 })
 export class PlaceDetailPage implements OnInit {
 
-  constructor(private router: Router, private navCtrl: NavController) { }
+  place: Place;
+  constructor(private navCtrl: NavController, private activatedRoute: ActivatedRoute, private placesService: PlacesService) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(paramMap=>{
+      if(!paramMap.has('placeId')){
+        this.navCtrl.navigateBack('/places/tabs/discover');
+        return;
+      }
+      this.place= this.placesService.getPlace(paramMap.get('placeId'));
+    });
   }
 
   onBookPlace(){
-    //this.router.navigateByUrl('/places/tabs/discover');
     this.navCtrl.navigateBack('/places/tabs/discover');
   }
 
