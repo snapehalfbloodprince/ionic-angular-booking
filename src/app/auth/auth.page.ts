761739500fea2f8af6298ef2fa99a2a0cebd3798
Francesco Paolo Piga <fppiga@outlook.com>
2021-01-30 18:05:59 +0100

    added LoadingController to auth page

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,21 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 import { AuthService } from './auth.service';
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.page.html',
-  styleUrls: ['./auth.page.scss'],
+  selector: "app-auth",
+  templateUrl: "./auth.page.html",
+  styleUrls: ["./auth.page.scss"],
 })
 export class AuthPage implements OnInit {
+  isLoading = false;
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private loadingController: LoadingController
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  onLogin(){
+  onLogin() {
+    this.isLoading = true;
     this.authService.login();
-    this.router.navigateByUrl('/places/tabs/discover');
+    this.loadingController
+      .create({ keyboardClose: true, message: "Logging in..." })
+      .then((loadingEl) => {
+        loadingEl.present();
+        setTimeout(() => {
+          this.isLoading = false;
+          loadingEl.dismiss();
+          this.router.navigateByUrl("/places/tabs/discover");
+        }, 1500);
+      });
+    
   }
 }
