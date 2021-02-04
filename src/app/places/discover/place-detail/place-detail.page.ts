d1d972da382cbb1deb544ee9f69864328349a155
Francesco Paolo Piga <fppiga@outlook.com>
2021-02-04 10:28:28 +0100

    added actionsheetcontroller to detail page

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,43 +1,78 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ModalController, NavController } from '@ionic/angular';
+import { ActionSheetController, ModalController, NavController } from '@ionic/angular';
 import { CreateBookingComponent } from '../../../bookings/create-booking/create-booking.component';
 import { Place } from '../../place.model';
 import { PlacesService } from '../../places.service';
 
 @Component({
-  selector: 'app-place-detail',
-  templateUrl: './place-detail.page.html',
-  styleUrls: ['./place-detail.page.scss'],
+  selector: "app-place-detail",
+  templateUrl: "./place-detail.page.html",
+  styleUrls: ["./place-detail.page.scss"],
 })
 export class PlaceDetailPage implements OnInit {
-
   place: Place;
-  constructor(private navCtrl: NavController, private activatedRoute: ActivatedRoute, private placesService: PlacesService, private modalController: ModalController) { }
+  constructor(
+    private navCtrl: NavController,
+    private activatedRoute: ActivatedRoute,
+    private placesService: PlacesService,
+    private modalController: ModalController,
+    private actionSheetController: ActionSheetController
+  ) {}
 
   ngOnInit() {
-    this.activatedRoute.paramMap.subscribe(paramMap=>{
-      if(!paramMap.has('placeId')){
-        this.navCtrl.navigateBack('/places/tabs/discover');
+    this.activatedRoute.paramMap.subscribe((paramMap) => {
+      if (!paramMap.has("placeId")) {
+        this.navCtrl.navigateBack("/places/tabs/discover");
         return;
       }
-      this.place= this.placesService.getPlace(paramMap.get('placeId'));
+      this.place = this.placesService.getPlace(paramMap.get("placeId"));
     });
   }
 
-  onBookPlace(){
+  onBookPlace() {
     // this.navCtrl.navigateBack('/places/tabs/discover');
-    this.modalController.create({component:CreateBookingComponent, 
-      componentProps: {selectedPlace:this.place}}).then(modalEl=>{
-      modalEl.present();
-      return modalEl.onDidDismiss();
-    })
-    .then(resultData=>{
-      console.log(resultData.data, resultData.role);
-      if(resultData.role==='confirm'){
-        console.log('BOOKED!');
-      }
+    this.actionSheetController.create({
+      header: "Choose an action",
+      buttons: [
+        {
+          text: "Select Date",
+          handler: ()=> {
+            this.openBookingModal("select");
+          }
+        },
+        {
+          text: "Random Date",
+          handler: ()=> {
+            this.openBookingModal("random");
+          }
+        },
+        {
+          text: "Cancel",
+          role: "cancel"
+        }
+      ]
+    }).then(actionSheetElement => {
+      actionSheetElement.present();
     });
   }
 
+  openBookingModal(mode: "select"|"random"){
+    console.log("Booked!");
+    this.modalController
+      .create({
+        component: CreateBookingComponent,
+        componentProps: { selectedPlace: this.place },
+      })
+      .then((modalEl) => {
+        modalEl.present();
+        return modalEl.onDidDismiss();
+      })
+      .then((resultData) => {
+        console.log(resultData.data, resultData.role);
+        if (resultData.role === "confirm") {
+          console.log("BOOKED!");
+        }
+      });
+  }
 }
